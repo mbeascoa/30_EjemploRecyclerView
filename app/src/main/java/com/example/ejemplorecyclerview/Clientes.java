32d275b6b74a4feaa5c5346009bb9e6e51c7369a
MIGUEL BEASCOA <77923795+mbeascoa@users.noreply.github.com>
2021-06-30 12:59:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ejemplorecyclerview/Clientes.java
@@ -0,0 +1,26 @@
+
+package com.example.ejemplorecyclerview;
+
+public class Clientes {
+
+    private String nombre;
+
+
+
+    public Clientes() {
+    }
+
+    public Clientes(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
